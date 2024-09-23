ddd0e9319c3ba1bd0f6d818559f352c7ce190278
ArthurSauerr <germanooarthuur@gmail.com>
2024-09-23 09:41:17 -0300

    feat: added cors policy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 app.use(express.json());
 
